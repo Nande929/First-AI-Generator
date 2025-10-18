3bee09851c700300cb34523c6cb90cb53551d85e
Nande929 <nandenqeto9@gmail.com>
2025-10-18 12:02:04 +0200

    added API

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-function generateQoute(event) {
+function displayQuotes(response) {
 
-event.preventDefault();
-
-new Typewriter('#quotes', {
-  strings: '"Beauty is in the eye of the beholder, \n\n No one is ugly"',
+  new Typewriter('#quotes', {
+  strings: `${response.data.answer}`,
   autoStart: true,
   cursor: null,
   delay: 1,
 });
+}
+
+
+
+function generateQoute(event) {
+
+event.preventDefault();
+
+let userInput = document.querySelector("#search-input");
+let prompt = `User Instruction: Generate a two line motivational quote about ${userInput.value}`
+let context = `You are an expert motivational speaker, that gives wholesome and very short and concise motivations. Write each line in basic HTML, using this format: <p></p> and should be inside quotation marks. Have a "SheCodes AI" signature at the end of the poem using this format: <strong></strong>`
+let apiKey = "e2b85d39ocbf2atcfebaabf3b2422057";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+axios.get(apiUrl).then(displayQuotes);
+
 }
 
 let form = document.querySelector("#quote-generator");
